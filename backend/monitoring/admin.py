d498ea332ff5e758c62bb5eeb71a43d7ba7ee605
Eric Turner <ericturner06@icloud.com>
2026-03-19 15:30:29 +0000

    Register models in admin interface

--- backend/monitoring/admin.py
+++ backend/monitoring/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Sites, Livestock, Readings, Alerts
+
+admin.site.register(Sites)
+admin.site.register(Livestock)
+admin.site.register(Readings)
+admin.site.register(Alerts)
