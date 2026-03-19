5784422d870999452c46eb765b08ad1703e5704b
Eric Turner <ericturner06@icloud.com>
2026-03-19 15:04:20 +0000

    Added full database to models.py

--- backend/monitoring/models.py
+++ backend/monitoring/models.py
@@ -5,6 +5,53 @@ from django.db import models
 class Sites(models.Model):
     name = models.CharField(max_length=100)
     geofence = models.CharField(max_length=100)
-    site_id = models.IntegerField()
+
     def __str__(self):
         return self.name
+
+class Livestock(models.Model):
+    name = models.CharField(max_length=100)
+    species = models.CharField(max_length=100)
+    site = models.ForeignKey(
+        Sites,
+        on_delete=models.CASCADE, # delete all livestock if their parent site is removed
+        related_name="livestock" # makes api design easier
+    )
+
+    def __str__(self):
+        return self.name
+
+class Readings(models.Model):
+    timestamp = models.DateTimeField()
+    livestock = models.ForeignKey(
+        Livestock,
+        on_delete=models.CASCADE,
+        related_name="readings",
+    )
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    acceleration = models.FloatField()
+    ambient_temperature = models.FloatField()
+    def __str__(self):
+        return self.timestamp
+
+class Alerts(models.Model):
+    timestamp = models.DateTimeField()
+    reading = models.ForeignKey(
+        Readings,
+        on_delete=models.CASCADE,
+        related_name="alerts",
+    )
+    livestock = models.ForeignKey(
+        Livestock,
+        on_delete=models.CASCADE,
+        related_name="alerts",
+    )
+    type = models.CharField(max_length=100)
+    description = models.TextField()
+
+
+
+
+    def __str__(self):
+        return f"{self.alert_type} at {self.timestamp}"
