b502b3d623a266f477608661560070f1e912fa35
compbolt <njdb4786@leeds.ac.uk>
2026-04-03 16:57:25 +0000

    fix settings again, missing django key

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
-    'monitoring'
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
