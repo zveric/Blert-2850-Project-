6f224be1f85e8fdb0fcdd024ed1a8f4e8985966c
Rudra Kumar Sharma <rudrakumar17sep.uk@gmail.com>
2026-04-29 17:20:24 +0100

    allowing port 5173 to request api data

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.gis',
     'monitoring',
-    'geofence',
+    #'geofence',
     'rest_framework', 
     'corsheaders',
 ]
@@ -131,7 +131,15 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
+# Letting rest api be open to requests so that the front end can access the data without token authentication
+# https://pypi.org/project/django-cors-headers/
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "http://127.0.0.1:5173",
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+}
