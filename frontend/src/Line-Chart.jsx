f594cfb14a42a8531fc81500df56803859d8573d
compbolt <njdb4786@leeds.ac.uk>
2026-05-02 18:30:30 +0000

    ts line chart begin

---
+++ frontend/src/Line-Chart.jsx
@@ -0,0 +1,6 @@
+import React, { useState } from 'react';
+import { Chart as ChartJS,CategoryScale,LinearScale,PointElement,LineElement,Title,Tooltip, Legend,} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+
+
