60dce4d3df3efb4d26c2396a272bce8743aed6ee
Mango <plais999@gmail.com>
2026-05-03 19:18:25 +0100

    Updated Map component
    Added a scrollbar below the map that allows you to switch between 50 different values from the database.

--- frontend/src/components/map.jsx
+++ frontend/src/components/map.jsx
@@ -10,6 +10,7 @@ import 'leaflet/dist/leaflet.css'
 function Map() {
     const [positionA, setPositionA] = useState(null)
     const [positionB, setPositionB] = useState(null)
+    const [sliderValue, setSliderValue] = useState(0)
 
     const customIcon = (color) => divIcon({
         className: '',
@@ -19,32 +20,47 @@ function Map() {
     })
 
     useEffect(() => {
-        getReadings(1, 1).then(data => {
-            const coords = data[0].geolocation.coordinates
-            console.log(coords)
-            setPositionA([coords[1], coords[0]])  // flip: [lat, lng]
+        Promise.all([ // Promise just allows the values to be there before we know them
+            getReadings(sliderValue, 1), // cow
+            getReadings(sliderValue, 2) //goat
+        ]).then(([dataA, dataB]) => {
+            if (dataA[0]) {
+                const coords = dataA[0].geolocation.coordinates
+                setPositionA([coords[1], coords[0]])  // flip: [lat, lng]
+            }
+            if (dataB[0]) {
+                const coords = dataB[0].geolocation.coordinates
+                setPositionB([coords[1], coords[0]])
+            }
         })
-
-        getReadings(1,2).then(data => {
-            const coords = data[0].geolocation.coordinates
-            console.log(coords)
-            setPositionB([coords[1], coords[0]])
-        })
-    }, [])
+    }, [sliderValue])
 
     if (!positionA) return <p>Loading map...</p>
     if (!positionB) return <p>Loading map...</p>
 
     return (
         // From leaflet docs
-        <MapContainer center={positionA} zoom={16} scrollWheelZoom={true} style={{ height: '500px', width: '100%' }}>
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={positionA} icon={customIcon('#2947cd')}></Marker>
-            <Marker position={positionB} icon={customIcon('#00a2b7')}></Marker>
-        </MapContainer>
+        <div style={{width: '100%'}}>
+            <MapContainer center={positionA} zoom={16} scrollWheelZoom={true} style={{ height: '500px', width: '100%' }}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={positionA} icon={customIcon('#2947cd')}></Marker>
+                <Marker position={positionB} icon={customIcon('#00a2b7')}></Marker>
+            </MapContainer>
+            <div>  
+                <label>sliderValue: {sliderValue}</label>
+                <input
+                    type="range"
+                    min="0"
+                    max="50"
+                    value={sliderValue}
+                    onChange={(e) => setSliderValue(e.target.value)}
+                    style={{width: '100%'}}
+                />
+            </div>
+        </div>
     )
 }
 
