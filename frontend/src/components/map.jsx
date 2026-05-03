2e3a47d5094f03f9e72276121c335d7e0473eecc
Rudra Kumar Sharma <rudrakumar17sep.uk@gmail.com>
2026-05-03 10:34:06 +0100

    added function to change the color of the marker icons

--- frontend/src/components/map.jsx
+++ frontend/src/components/map.jsx
@@ -1,13 +1,22 @@
 import { useState, useEffect } from 'react'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-import 'leaflet/dist/leaflet.css'
 import { getReadings } from '../api'
+import { divIcon } from 'leaflet'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import 'leaflet/dist/leaflet.css'
 
 
 
 function Map() {
     const [positionA, setPositionA] = useState(null)
     const [positionB, setPositionB] = useState(null)
+
+    const customIcon = (color) => divIcon({
+        className: '',
+        html: `<i class="fa-solid fa-location-dot" style="color: ${color}; font-size: 32px;"></i>`,
+        iconSize: [32, 32],
+        iconAnchor: [16, 16],
+    })
 
     useEffect(() => {
         getReadings(1, 1).then(data => {
@@ -33,16 +42,8 @@ function Map() {
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={positionA}>
-                <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-            </Marker>
-            <Marker position={positionB}>
-                <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-            </Marker>
+            <Marker position={positionA} icon={customIcon('#2947cd')}></Marker>
+            <Marker position={positionB} icon={customIcon('#00a2b7')}></Marker>
         </MapContainer>
     )
 }
