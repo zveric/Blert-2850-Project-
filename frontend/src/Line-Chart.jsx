61f05ac86560a2ffa7bfa2304f03d31ea21ae942
compbolt <njdb4786@leeds.ac.uk>
2026-05-03 13:06:39 +0000

    finished graph stuff ready for testing?

--- frontend/src/Line-Chart.jsx
+++ frontend/src/Line-Chart.jsx
@@ -1,25 +1,48 @@
 import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
 import { Line } from 'react-chartjs-2';
+import { getReadings } from '../api';
+import { useState, useEffect } from 'react';
 
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
 export default function LineChart() {
+  const [temperatures, setTemperatures] = useState([]);
+  const [labels, setLabels] = useState([]);
+
+  useEffect(() => {
+    getReadings(1, 1).then(data => {
+      
+      const temps = data.map(item => item.ambient_temperature_c);
+      console.log(temps); 
+      setTemperatures(temps);
+
+      const times = data.map((item, index) => {
+        return item.timestamp 
+          ? new Date(item.timestamp).toLocaleTimeString() 
+          : `Reading ${index + 1}`;
+      });
+      
+      setLabels(times);
+      
+    }); 
+  }, []);
+
   return (
     <Line
       datasetIdKey='id'
       data={{
-        labels: ['Jun', 'Jul', 'Aug'],
+        labels: labels, 
         datasets: [
           {
             id: 1,
-            label: 'Dataset 1',
-            data: [5, 6, 7],
+            label: 'temp1 1',
+            data: temperatures, 
             borderColor: 'rgb(0, 30, 255)', 
           },
           {
             id: 2,
-            label: 'Dataset 2',
-            data: [3, 2, 1],
+            label: 'temp 2',
+            data: temperatures,
             borderColor: 'rgb(255, 72, 0)',
           },
         ],
