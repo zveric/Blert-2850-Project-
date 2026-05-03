91e7a6b05e8783b03e02411bc3916c06920cd21c
compbolt <njdb4786@leeds.ac.uk>
2026-05-03 13:41:55 +0000

    added the goat (lebron?)

--- frontend/src/components/Line-Chart.jsx
+++ frontend/src/components/Line-Chart.jsx
@@ -7,6 +7,7 @@ ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, T
 
 export default function LineChart() {
   const [temperatures, setTemperatures] = useState([]);
+  const [temperatures2, setTemperatures2] = useState([]);
   const [labels, setLabels] = useState([]);
 
   useEffect(() => {
@@ -24,11 +25,28 @@ export default function LineChart() {
       });
       
       setLabels(times);
+    });
+
+      getReadings(50, 2).then(data => {
+      data = data.reverse();
+      
+      const temps2 = data.map(item => item.ambient_temperature_c);
+      console.log(temps2); 
+      setTemperatures2(temps2);
+
+      const times = data.map((item, index) => {
+        return item.timestamp 
+          ? new Date(item.timestamp).toLocaleTimeString() 
+          : `Reading ${index + 1}`;
+      });
+      
+      setLabels(times);
       
     }); 
   }, []);
 
   return (
+    <div style={{ height: '400px', width: '100%' }}>
     <Line
       datasetIdKey='id'
       data={{
@@ -36,12 +54,19 @@ export default function LineChart() {
         datasets: [
           {
             id: 1,
-            label: 'temp1 1',
+            label: 'herd1: cow',
             data: temperatures, 
             borderColor: 'rgb(0, 30, 255)', 
+          },
+                    {
+            id: 2,
+            label: 'herd2: goat',
+            data: temperatures2,
+            borderColor: 'rgb(255, 72, 0)',
           },
         ],
       }}
     />
+    </div>
   );
 }
