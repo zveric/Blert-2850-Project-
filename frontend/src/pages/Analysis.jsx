d40b1363c7670a371f145cdbb38a1166e84a4e59
Eric Turner <ericturner06@icloud.com>
2026-05-04 18:51:59 +0100

    Add routing and new pages for Analysis and Dashboard

---
+++ frontend/src/pages/Analysis.jsx
@@ -0,0 +1,15 @@
+import Navbar from '../components/navbar'
+
+function Analysis() {
+    return (
+        <>
+
+            <main style={{ padding: '2rem' }}>
+                <h1>Analysis</h1>
+                <p>This is the analysis page.</p>
+            </main>
+        </>
+    )
+}
+
+export default Analysis
---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,46 @@
+import {useState} from "react";
+import {windowBreakpoints} from "../components/windowBreakpoints.js";
+import Map from "../components/map.jsx";
+import AlertBtn from "../components/alert-btn.jsx";
+import LineChart from "../components/Line-Chart.jsx";
+import ReadingList from "../components/readings-list.jsx";
+
+function Dashboard() {
+    const [count, setCount] = useState(0)
+
+    const { width, height,isMobile } = windowBreakpoints();
+
+    const pageStyle = {
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '20px',
+    };
+
+    const mapAndAlertStyle = {
+        display: 'flex',
+        flexDirection: isMobile ? 'column' : 'row',
+        gap: '20px',
+
+        margin: isMobile ? "0 0px" : "0 150px",
+    };
+
+    return (
+        <section style={pageStyle} id="Dashboard">
+            <div style={mapAndAlertStyle}>
+                <Map />
+                <div style={{width : "30%"}}>
+                    <AlertBtn />
+                    <LineChart/>
+
+                </div>
+            </div>
+            <div style={{gap: "20px", padding: "0 50px",margin: isMobile ? "0 0px" : "0 150px",}}>
+                <ReadingList/>
+            </div>
+            <div style={{padding: "0 50px"}}></div>
+
+        </section>
+    )
+}
+
+export default Dashboard
