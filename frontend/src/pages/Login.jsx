c39c1a10a98ab182886064abde7bf373048c7134
Mango <plais999@gmail.com>
2026-05-08 00:33:32 +0100

    Updated the Login and Register pages
    Changed the register and login page to look consistent, and be more accessible. Added better keyboard functionality, as well as resizing the logo.

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,13 +2,26 @@
 import { useNavigate } from 'react-router-dom'
 import { login } from '../api'
 import logoImage from '../assets/logo.jpeg'
+import {windowBreakpoints} from "../components/windowBreakpoints.js";
 
 function Login() {
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
     const [error, setError] = useState(null)
+    const { isMobile } = windowBreakpoints();
 
     const navigate = useNavigate()
+
+    const cardStyle = {
+        background: "#fff",
+        borderRadius: "16px",
+        boxShadow: "0 4px 16px rgba(0,0,0,0.10)",
+        display: "inline-block",
+        width: '30vw',
+        height: "100%",
+        overflow: "hidden",  
+        padding: "40px",
+    }
 
     const handleLogin = () => {
         login(username, password).then(success => {
@@ -20,24 +33,26 @@ function Login() {
         })
     }
 
+    
+    
     return (
         <div className="container mt-5">
             <div className="row justify-content-center">
-                <div className="col-md-4">
+                <div className="col-md-4" style={cardStyle}>
 
-                    <img src={logoImage} alt="Blert Logo" className="rounded mb-2" />
-                    <h2>Blert Login</h2>
+                    <img src={logoImage} alt="Blert Logo" className="rounded mb-2" style={{ width: '75%', height: 'auto', display: 'block', margin: 'auto auto' }} />
+                    <h2 style={{ fontWeight: 'bold' }}>Login</h2>
 
                     {error && <p style={{ color: 'red' }}>{error}</p>}
 
                     <div className="mb-3">
                         <label className="form-label">Username</label>
-                        <input type="text" className="form-control" value={username} onChange={e => setUsername(e.target.value)} />
+                        <input type="text" className="form-control" value={username} onChange={e => setUsername(e.target.value)} onKeyDown={e => {if (e.key === 'Enter') document.getElementById('password-input').focus()} } />
                     </div>
 
                     <div className="mb-3">
                         <label className="form-label">Password</label>
-                        <input type="password" className="form-control" value={password} onChange={e => setPassword(e.target.value)} />
+                        <input type="password" className="form-control" id="password-input" value={password} onChange={e => setPassword(e.target.value)} onKeyDown={e => {if (e.key === 'Enter') handleLogin()} } />
                     </div>
 
                     <button className="btn btn-dark" onClick={handleLogin}>Login</button>
